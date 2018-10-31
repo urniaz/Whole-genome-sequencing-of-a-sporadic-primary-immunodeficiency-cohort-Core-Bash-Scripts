774a3976dbbad47f33beba0fe5904b17b1544119
Rafal Urniaz, PhD <RafalUrniaz@users.noreply.github.com>
2018-10-31 20:41:26 +0100

    Add files via upload

---
+++ EE_interactive_sample_filtering_20170104-B_merged-vcf_no_hgmd_SNPtest_case_control.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+source set_environment.sh
+
+### Chromosome of interest number
+
+CHR=$@
+
+echo "Chromosome of interest number $CHR"
+
+### Input VCF
+IN="$WORKING_DIR"GEN/gs10k_20170104-B_VCF_gnomad_exclude_less_0.01_MAF_exclude_less_0.05_no_indels_chr_"$CHR".gen
+
+echo "IN="$IN
+
+## Output file
+OUT="$WORKING_DIR"SNPTEST/gs10k_20170104-B_VCF_gnomad_exclude_less_0.01_MAF_exclude_less_0.05_no_indels_SNPtest_chr_"$CHR"_case_553_control_5242_AD_ex_monogenic_Eur_only
+
+echo "OUT="$OUT
+
+## Sample file
+
+SAMPLE=/scratch/ru222/data_20170104-B_update/sample_files/CaseControl/snptest_sample_file_case_553_control_5242_AD_ex_monogenic_Eur_only.sample
+
+echo "SAMPLE="$SAMPLE
+
+## When start ?
+
+echo "chr_""$CHR"": Start :"
+date
+
+snptest -data $IN $SAMPLE -o $OUT -cov_all -frequentist 1 -method score -pheno PID_case -hwe | tee "$OUT"_console_output.txt
+
+## Status
+echo "chr_""$CHR"": Done"
+
+## When finished ?
+
+echo "chr_""$CHR"": Finished :"
+date
+
+#* * * * *
+
+#You then submit this script with
+
+#sbatch --array=0-22 <sbatch_script>
+
+# or interactive model parallely
+
+# parallel -q --nice 19 bash  {} ::: {1..22}
